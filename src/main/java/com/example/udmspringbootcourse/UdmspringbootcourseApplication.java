5ddc8e9bad0a9e883da9237195dd5023385a95f3
alec <alecrondel@hotmail.com>
2023-10-02 20:01:55 -0300

    Project created

---
+++ src/main/java/com/example/udmspringbootcourse/UdmspringbootcourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.udmspringbootcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdmspringbootcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdmspringbootcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/udmspringbootcourse/UdmspringbootcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udmspringbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdmspringbootcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
